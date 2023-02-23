82099974e6105462f0606c5e9775e2223d3cdb1f
josdaperales <josdaperales@gmail.com>
2023-02-23 17:53:17 -0500

    consumiendo api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [mensaje, setMensaje] = useState("");
+
+  async function fetchFunctionHola() {
+    const response = await axios.get(
+      "https://funcion-hola-mundo-u7nfcrh3jq-uc.a.run.app"
+    );
+    console.log(response);
+    setLoading(false);
+    return response.data;
+  }
+
+  useEffect(() => {
+    fetchFunctionHola().then((e) => {
+      setMensaje(e);
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Cargando...</h1>
+      </div>
+    );
+  }
   return (
     <main>
-      <section className='container'>
-        <div className='title'>
-          <h2>Hello Hijar xddd</h2>
-          <div className='underline'></div>
+      <section className="container">
+        <div className="title">
+          <h2>{mensaje}</h2>
+          <div className="underline"></div>
         </div>
         <Review />
       </section>
